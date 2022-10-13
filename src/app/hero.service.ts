0b9574725048d8866a21904ad3e909a97767f7ef
Anthony Pena <apena5@alumni.jh.edu>
2022-10-13 17:22:56 -0600

    feat: update hero name

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,6 +12,10 @@ import { catchError, map, tap } from 'rxjs/operators';
 export class HeroService {
   
   private heroesUrl = 'api/heroes';
+  
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(
     private http: HttpClient,
@@ -26,11 +30,18 @@ export class HeroService {
   }
   
   getHero(id: number): Observable<Hero>{
-    const url = `{this.heroesUrl}/${id}`;
+    const url = `${this.heroesUrl}/${id}`;
     return this.http.get<Hero>(url).pipe(
         tap(_ => this.log(`fetched hero id=${id}`)),
         catchError(this.handleError<Hero>(`getHero id=${id}`))
       );
+  }
+  
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
   }
   
   private handleError<T>(operation = 'operation', result?: T){
