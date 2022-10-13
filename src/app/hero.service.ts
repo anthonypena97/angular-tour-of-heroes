4dd4b7e16b289059f59cae79212a2276bbf44140
Anthony Pena <apena5@alumni.jh.edu>
2022-10-13 15:57:44 -0600

    feat: simulate a data server - get heroes from HttpClient

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,24 +3,33 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of} from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  
+  private heroesUrl = 'api/heroes';
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+    ) { }
   
   getHeroes(): Observable<Hero[]>{
-    const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
   
   getHero(id: number): Observable<Hero>{
     const hero = HEROES.find(h => h.id === id)!;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
+  }
+  
+  private log(message: string){
+    this.messageService.add(`HeroService: ${message}`);
   }
   
 }
